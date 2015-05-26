276a2409224389fb8076a51d4fa0216d4c30d017
Tighe Kuykendall <tkuykend@cisco.com>
2015-05-26 10:30:00 -0400

    Added cli arguments and additional output info.

--- FN63894-CheckPsu.py
+++ FN63894-CheckPsu.py
@@ -1,37 +1,95 @@
 #!/usr/bin/python
+#
+# Check for FN63894 PSU issue on 6296 Fabric Interconnects
+#
+# By Rusty Buzhardt
+#
+
+
+
 
 from pprint import pprint
 from UcsSdk import *
 from UcsSdk.MoMeta.EquipmentPsu import EquipmentPsu
+import getpass
+import optparse
+import platform
+
+
+def getpassword(prompt):
+  if platform.system() == "Linux":
+    return getpass.unix_getpass(prompt=prompt)
+  elif platform.system() == "Windows" or platform.system() == "Microsoft":
+    return getpass.win_getpass(prompt=prompt)
+  elif platform.system() == "Macintosh":
+    return getpass.unix_getpass(prompt=prompt)
+  else:
+    return getpass.getpass(prompt=prompt)
+
+
 
 if __name__ == "__main__":
-   try:
-      handle = UcsHandle()
-      IP = raw_input("FI IP address: ")
-      username = raw_input("Username: ")
-      import getpass
-      password = getpass.getpass("Password: ")
+  try:
 
-      handle = UcsHandle()
-      handle.Login(IP, username, password)
-      for psu in handle.GetManagedObject(None, EquipmentPsu.ClassId()):
-          if ("sys/switch" in psu.Dn) and ("UCS-PSU-6296UP-AC" in psu.Model):
-             if "-A0" in psu.Revision:
-                 print "Please contact Cisco TAC.  The PSU in " + psu.Dn + \
-                       " needs to be replaced per FN63894.  More information can be found here:\n" +  \
-                       "http://www.cisco.com/c/en/us/support/docs/field-notices/638/fn63894.html"
-             elif ("-B0" in psu.Revision) and ("UCS-PSU-6296UP-AC" in psu.Model):
-                 print "Please contact Cisco TAC.  The PSU in " + psu.Dn + \
-                       " needs to be replaced per FN63894.  More information can be found here:\n" + \
-                       "http://www.cisco.com/c/en/us/support/docs/field-notices/638/fn63894.html"
-             else:
-                print "Thepower supply in " + psu.Dn + " is not affected by FN63894."
-      handle.Logout()
+    parser = optparse.OptionParser()
+    parser.add_option('-i', '--ip', dest="ip", help="UCSM IP Address")
+    parser.add_option('-u', '--username', dest="userName", help="Read Only Username For UCS Manager")
+    parser.add_option('-p', '--password', dest="password", help="Read Only Password For UCS Manager")
+    (options, args) = parser.parse_args()
 
-   except Exception, err:
-      print "Exception:", str(err)
-      import traceback, sys
-      print '-'*60
-      traceback.print_exc(file=sys.stdout)
-      print '-'*60
-      handle.Logout()
+    # Check for a command line FI IP, if not prompt for it.
+
+    if options.ip:
+      print "Connecting to UCS Manager at address " + options.ip
+    elif not options.ip:
+      options.ip = raw_input("UCS Manager IP Address: ")
+
+    # Check for a command line username, if not prompt for it.
+
+    if options.userName:
+      print "Logging in as " + options.userName
+    elif not options.userName:
+      options.userName = raw_input("UCS Manager Read Only Username: ")
+
+    # Check for a command line password, if not prompt for it.
+
+    if options.password:
+      print "Thanks for providing the password."
+    elif not options.password:
+      options.password = getpassword("UCS Manager Password: ")
+
+
+    handle = UcsHandle()
+    handle.Login(options.ip, options.userName, options.password)
+
+      # Check for the effected PSU.
+
+    print "Checking..."
+    for psu in handle.GetManagedObject(None, EquipmentPsu.ClassId()):
+        if ("sys/switch" in psu.Dn) and ("UCS-PSU-6296UP-AC" in psu.Model):
+           if "-A0" in psu.Revision:
+               print "Please contact Cisco TAC.  The PSU in " + psu.Dn + \
+                     " needs to be replaced per FN63894.  "
+           elif ("-B0" in psu.Revision) and ("UCS-PSU-6296UP-AC" in psu.Model):
+               print "Please contact Cisco TAC.  The PSU in " + psu.Dn + \
+                     " needs to be replaced per FN63894."
+           else:
+              print "Thepower supply in " + psu.Dn + " is not affected by FN63894."
+        else:
+            print ".",
+    print "Done"
+    print ""
+    print "If you have a PSU effected by FN63894, More information can be found here:\n"
+    print "http://www.cisco.com/c/en/us/support/docs/field-notices/638/fn63894.html"
+    print ""
+    print ""
+
+    handle.Logout()
+
+  except Exception, err:
+    print "Exception:", str(err)
+    import traceback, sys
+    print '-'*60
+    traceback.print_exc(file=sys.stdout)
+    print '-'*60
+    handle.Logout()
